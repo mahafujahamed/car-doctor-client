87188a3e3d6b208eeb28b678c1d96d11f7bccea9
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-07-14 02:23:57 +0600

    loaded json data

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -18,7 +18,7 @@ const Services = () => {
                 <h2 className="text-5xl">Our Service Area</h2>
                 <p>The majority suffered alteration in some form, by injected , or randomised <br /> words which do not look slightly beleivable</p>
             </div>
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                {
                 services.map(service => <ServiceCard
                 key={service._id}
