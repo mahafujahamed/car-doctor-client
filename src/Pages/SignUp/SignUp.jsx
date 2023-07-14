976103e9d6a346e70cfb83df8e9b20ac417b056f
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-07-14 12:19:40 +0600

    added sign up form

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,8 +1,8 @@
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import img from '../../assets/images/login/login.svg';
-const Login = () => {
-    const handleLogin = event => {
+
+const SignUp = () => {
+    const handleSignUp = event => {
         event.preventDefault();
     }
     return (
@@ -13,8 +13,14 @@ const Login = () => {
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                        <h1 className="text-3xl text-center text-orange-400 font-bold">Login</h1>
-                        <form onSubmit={handleLogin}>
+                        <h1 className="text-3xl text-center text-orange-400 font-bold">Sign Up</h1>
+                        <form onSubmit={handleSignUp}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input type="text" name='name' placeholder="Name" className="input input-bordered" />
+                            </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
@@ -23,7 +29,7 @@ const Login = () => {
                             </div>
                             <div className="form-control">
                                 <label className="label">
-                                    <span className="label-text">Password</span>
+                                    <span className="label-text"> Confirm Password</span>
                                 </label>
                                 <input type="text" name='password' placeholder="password" className="input input-bordered" />
                                 <label className="label">
@@ -31,10 +37,10 @@ const Login = () => {
                                 </label>
                             </div>
                             <div className="form-control mt-6">
-                                <input className="btn btn-primary" type="submit" value="Login" />
+                                <input className="btn btn-primary" type="submit" value="Sign Up" />
                             </div>
                         </form>
-                        <p className='my-4 text-center'>New to Car Doctors <Link className='text-orange-400 font-bold' to="/signup">Sign Up</Link></p>
+                        <p className='my-4 text-center'>Already Have an ccount? <Link className='text-orange-400 font-bold' to="/login">Login</Link></p>
                     </div>
                 </div>
             </div>
@@ -42,4 +48,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
+import SignUp from "../Pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
         {
           path: "login",
           element: <Login></Login>
+        },
+        {
+          path: "signup",
+          element: <SignUp></SignUp>
         }
       ]
     },
