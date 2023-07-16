822cb9d9e74708ffbdbfe455af0760abdf448dd8
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-07-17 03:34:04 +0600

    creare checkout and linked

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 
 
-const ServiceCard = ({service}) => {
-    const {title, img, price} = service;
+const ServiceCard = ({ service }) => {
+    const { _id, title, img, price } = service;
 
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
@@ -10,7 +11,9 @@ const ServiceCard = ({service}) => {
                 <h2 className="card-title">{title}</h2>
                 <p>Price: ${price}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <Link to={`/checkout/${_id}`}>
+                        <button className="btn btn-primary">Book Now</button>
+                    </Link>
                 </div>
             </div>
         </div>
