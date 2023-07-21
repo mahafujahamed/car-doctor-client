e6eb9c9a86251464281ccd05b1b6188f84f9db38
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-07-21 22:28:16 +0600

    added checkout page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import CheckOut from "../Pages/Checkout/CheckOut";
 
 const router = createBrowserRouter([
     {
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
         {
           path: "signup",
           element: <SignUp></SignUp>
+        },
+        {
+          path: "checkout/:id",
+          element: <CheckOut></CheckOut>,
+          loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
         }
       ]
     },
